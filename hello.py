f36909db4da486f5dcb58ab1fd601b1fcd57f9e8
jaymarc21 <>
2025-03-08 00:59:23 +0800

    Last Sync: 2025-03-07 16:59:23 (Mobile)

--- hello.py
+++ hello.py
@@ -1,13 +1,8 @@
-print("Hello world!")
+
 print("Hello world!")
 print("Name: [Jay Marc Custodio]")
 print("Course: [BSIT]")
-print("Interests in programming: [Your Interests]")
-python3 hello.py > output.txt
+print("Interests in programming: [Web Development]")
 
-if grep -q "Hello world!" output.txt && grep -qF "$(cat intro.txt)" output.txt; then
-    echo "Output Passed"
-else
-    echo "Output Failed"
-    exit 1
-fi
+
+
