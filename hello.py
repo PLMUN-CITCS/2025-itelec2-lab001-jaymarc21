8350ccb2962d7926bdce7b4e9af08721823047f5
jaymarc21 <>
2025-03-08 00:54:56 +0800

    Last Sync: 2025-03-07 16:54:56 (Mobile)

--- hello.py
+++ hello.py
@@ -3,3 +3,11 @@ print("Hello world!")
 print("Name: [Jay Marc Custodio]")
 print("Course: [BSIT]")
 print("Interests in programming: [Your Interests]")
+python3 hello.py > output.txt
+
+if grep -q "Hello world!" output.txt && grep -qF "$(cat intro.txt)" output.txt; then
+    echo "Output Passed"
+else
+    echo "Output Failed"
+    exit 1
+fi
